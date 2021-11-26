ab9745ff8f8523c236e26b6f884921071731b834
Entgelmeier1 <d.entgelmeier@ceyoniq.com>
2021-11-26 13:06:01 +0100

    Add example to remove CR and LF characters from a string

--- src/main/java/airhacks/App.java
+++ src/main/java/airhacks/App.java
@@ -9,5 +9,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println( "Hello Java 17!" );
+        // Example: Remove all CR and LF characters from a string
+        String s = "Line one. \nLine two. \rLine three. \n\rLine four. \r\nLine five.";
+        System.out.println( s.replaceAll("[\n\r]", "") );
     }
 }
