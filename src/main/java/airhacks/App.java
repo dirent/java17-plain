7af01893f0be46aa2b861702fbea626f70e83072
Entgelmeier1 <d.entgelmeier@ceyoniq.com>
2022-01-12 08:14:02 +0100

    Add example to format a list of strings with collector

--- src/main/java/airhacks/App.java
+++ src/main/java/airhacks/App.java
@@ -1,6 +1,9 @@
 package airhacks;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author airhacks.com
@@ -12,5 +15,11 @@ public class App {
         // Example: Remove all CR and LF characters from a string
         String s = "Line one. \nLine two. \rLine three. \n\rLine four. \r\nLine five.";
         System.out.println( s.replaceAll("[\n\r]", "") );
+        // Example: Formatting string lists with collector
+        // http://adambien.blog/roller/abien/entry/formatting_string_lists_with_collector
+        var words = List.of( "eins", "zwei", "drei" );
+        var joined = words.stream().collect(
+                Collectors.joining("\", \"", "[ \"", "\" ]") );
+        System.out.println( "Joined: " + joined );
     }
 }
