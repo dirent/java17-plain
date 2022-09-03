30ce08ec98aa40779ac0bdd9ce9930289450f559
Entgelmeier1 <d.entgelmeier@ceyoniq.com>
2022-09-03 14:59:53 +0200

    Brückentag: Read public holiday from csv

--- src/main/java/de/dirent/Brueckentage.java
+++ src/main/java/de/dirent/Brueckentage.java
@@ -4,7 +4,13 @@ import org.joda.time.*;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class Brueckentage {
 
@@ -16,11 +22,36 @@ public class Brueckentage {
         DateTimeFormatter fmt = DateTimeFormat.forPattern("E dd.MM.yyyy");
         LocalDate day = start;
         while(day.isBefore(end) ) {
-            System.out.println( fmt.print(day) );
             day = day.plusDays(1);
         }
 
         Days _2023 = Days.daysBetween( start, end );
-        System.out.println( "2023 hat " + _2023.getDays() + " Tage.");
+        System.out.println( "Days count: " + _2023.getDays() );
+
+        List<LocalDate> feiertage = readFeiertage();
+        System.out.println( "Read " + feiertage.size() + " public holidays." );
+    }
+
+    public static List<LocalDate> readFeiertage() {
+        List<LocalDate> feiertage = new ArrayList<>();
+        InputStream csvStream = Brueckentage.class.getResourceAsStream("nrw.csv");
+        if( csvStream == null ) {
+            System.err.println( "No input csv found." );
+            return feiertage;
+        }
+        try( Scanner csv = new Scanner(csvStream) ) {
+
+            // skip first line containing the heading
+            if( csv.hasNextLine() ) csv.nextLine();
+
+            DateTimeFormatter dtf = DateTimeFormat.forPattern("dd.MM.yyyy");
+            while( csv.hasNextLine() ) {
+                String[] parts = csv.nextLine().split(";");
+                if( parts.length == 2 ) {
+                    feiertage.add( dtf.parseLocalDate(parts[0]) );
+                }
+            }
+        }
+        return feiertage;
     }
 }
